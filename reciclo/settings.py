9333ef62439f2cdfd8c1a4b0559e74bd2be85244
kenjork <genio.creative@gmail.com>
2018-11-18 12:48:38 -0500

    version 2.1.7

--- reciclo/settings.py
+++ reciclo/settings.py
@@ -171,10 +171,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-if not DEBUG:
-    # Update database configuration with $DATABASE_URL.
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
